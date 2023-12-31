59569b093e3b96b8b69c0515534b79038d9eddce
RajeshCodingBlck <137406636+RajeshCodingBlck@users.noreply.github.com>
2023-12-31 23:46:12 +0530

    Create Pattern3.java

---
+++ Lec_6/Pattern3.java
@@ -0,0 +1,46 @@
+package Lec_6;
+import java.util.*;
+public class Pattern1 {
+    
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      
+		Scanner s= new Scanner(System.in);
+		int n= s.nextInt();
+		
+		int row=1;
+		int nst=1;
+		int nsp=n-1;
+		while(row<=n) {
+			// Print the Space
+			int i=1;
+			while(i<=nsp) {
+				System.out.print(" "+" ");
+				i=i+1;
+			}
+			// Print the Star
+			int j=1;
+			while(j<=nst) {
+				
+				if(j%2==0) {
+					System.out.print(" "+" ");
+				}else {
+					System.out.print("*"+" ");
+				}
+				j=j+1;
+			}
+			// Preparation for the Next Row
+			nsp=nsp-1;
+			nst=nst+2;
+			
+			// Go for Next Row
+			row=row+1;
+			System.out.println();
+			
+		}
+		
+		
+	}
+
+}
