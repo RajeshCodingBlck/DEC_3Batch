0dd76a1c43dfbe0bd4e9287181b4663ff8fc5753
RajeshCodingBlck <137406636+RajeshCodingBlck@users.noreply.github.com>
2024-04-06 12:20:55 +0530

    Add files via upload

---
+++ Revision_3/Revision/Break_and_contiue.java
@@ -0,0 +1,22 @@
+package Revision;
+
+public class Break_and_contiue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+		for(int j=0; j<=3;j++) {
+			
+			for(int i=0; i<10;i++) {
+				
+				if(i==3) {
+					break;
+				}
+				System.out.println(i);
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
