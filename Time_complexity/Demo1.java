45acc363262acafded4bd2b6fdbe1f5af527a0d9
RajeshCodingBlck <137406636+RajeshCodingBlck@users.noreply.github.com>
2024-04-21 13:16:39 +0530

    Add files via upload

--- Time_complexity/Demo1.java
+++ Time_complexity/Demo1.java
@@ -4,7 +4,24 @@ public class Demo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        int n=1000000;
+        
+		for(int i=1; i<=n;i*=2) {
+			
+			System.out.println("Hello");
+		}
+		
+		
+		for(int i=n;i>=1; i=i/2) {
+			
+			System.out.println("hello");
+		}
+		
+		
+		
+		
+		
+		
 	}
 
 }
---
+++ Time_complexity/Demo_2.java
@@ -0,0 +1,10 @@
+package Time_complexity;
+
+public class Demo_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
