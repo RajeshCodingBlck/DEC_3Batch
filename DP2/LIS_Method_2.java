8bd289c7b33996db6aabc6a5851700126176b70c
RajeshCodingBlck <137406636+RajeshCodingBlck@users.noreply.github.com>
2024-07-09 19:47:57 +0530

    Create LIS_Method_2.java

---
+++ DP2/LIS_Method_2.java
@@ -0,0 +1,50 @@
+class Solution {
+    
+    public int BS(int [] dp, int val){
+        
+        
+      int low=1;
+      int high= dp.length-1;
+        
+       int ans=-1;
+       while(low<=high){
+           
+           int mid= low+ (high-low)/2;
+           
+           if(dp[mid]>=val){
+               ans=mid;
+               high=mid-1;
+           }else{
+               low=mid+1;
+           }
+       } 
+        return ans;
+    }
+    public int lengthOfLIS(int[] nums) {
+       
+      int n= nums.length;
+      int [] dp= new int[n+1];
+    // dp[i]= Minimum Ending value I can store to Create i length Increasing Subsequence
+      for(int i=0; i<dp.length;i++){
+          dp[i]= (int)(1e9);
+      }
+        
+      for(int i=0; i<nums.length;i++){
+          
+          int index= BS(dp, nums[i]);
+          dp[index]=nums[i];
+      }
+        
+    for(int i=dp.length-1; i>=1;i--){
+        
+        if(dp[i] != (int)(1e9)){
+            return i;
+        }
+    }
+        
+        return 1;
+        
+    
+        
+    }
+}
